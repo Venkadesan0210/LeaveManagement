9d8f2f6c220cd464f5d71715f3fb4f58760961ce
Venkadesan0210 <venkadesan.jothimani@zucisystems.com>
2024-03-25 15:37:54 +0530

    Initial folder setup for application

--- LeaveManagement/LeaveManagement/src/main/java/com/example/LeaveManagement/LeaveManagementApplication.java
+++ LeaveManagement/LeaveManagement/src/main/java/com/example/LeaveManagement/LeaveManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.LeaveManagement")
 public class LeaveManagementApplication {
 
 	public static void main(String[] args) {
--- LeaveManagement/LeaveManagement/src/main/java/com/example/LeaveManagement/users/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.LeaveManagement.users.Repo;
-
-import com.example.LeaveManagement.users.UserEntity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-    User findByUsername(String username);
-}
