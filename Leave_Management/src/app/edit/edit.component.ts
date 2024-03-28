3f77f431d23a82f4a96f30ad9d92e73b419ade5c
Venkadesan0210 <venkadesan.jothimani@zucisystems.com>
2024-03-28 17:52:48 +0530

    Adding the UI for all modules and implement the functionality

--- Leave_Management/src/app/edit/edit.component.ts
+++ Leave_Management/src/app/edit/edit.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UrlService } from '../Services/url.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit',
@@ -6,5 +11,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent {
+UserEmail: any;
+ManagerEmail: any;
+userForm!: FormGroup;
+constructor(private urlService:UrlService,private router: Router,private toastr: ToastrService,private fb: FormBuilder ) {}
+ngOnInit() {
+  this.userForm = this.fb.group({
+    useremail: ['', [Validators.required, Validators.email]],
+    manageremail: ['', [Validators.required, Validators.email]]
+  });
+}
 
+  sendData(){
+
+    const assignedManger = {
+      managerUserName: this.ManagerEmail,
+      userName: this.UserEmail,
+    }
+
+    this.urlService.assigenedManager(assignedManger).subscribe({
+      next: (response: any) => {
+        this.router.navigate(['/home']);
+
+      },
+      error: (error: any) => {
+        console.error('Error fetching user details:', error);
+      }
+    });
+  }
+  onSubmit(){
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
+  logout(){
+    this.router.navigate(['/home']);
+
+  }
 }
